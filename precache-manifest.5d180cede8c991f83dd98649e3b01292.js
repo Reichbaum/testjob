da913bb31a66c13e95c87be7faabd9db921db5c3
Юлия <bigmattres@ya.ru>
2020-08-16 11:44:31 +0300

    Updates

---
+++ precache-manifest.5d180cede8c991f83dd98649e3b01292.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e909016d535fc3072c84755303de4250",
+    "url": "/testjob/index.html"
+  },
+  {
+    "revision": "b9c10740e092282c024e",
+    "url": "/testjob/static/css/main.697fae84.chunk.css"
+  },
+  {
+    "revision": "c12b38ca52e4fa22cdb5",
+    "url": "/testjob/static/js/2.2b98640d.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/testjob/static/js/2.2b98640d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c10740e092282c024e",
+    "url": "/testjob/static/js/main.1acad8e0.chunk.js"
+  },
+  {
+    "revision": "5bfb4ecedcc20196d75f",
+    "url": "/testjob/static/js/runtime-main.65693edc.js"
+  },
+  {
+    "revision": "2167a1084076ce00b1bddd7d0b913f59",
+    "url": "/testjob/static/media/reich.2167a108.jpg"
+  },
+  {
+    "revision": "e3bf0b87e55ed028f06733017f1b842f",
+    "url": "/testjob/static/media/reichbaum-logo.e3bf0b87.svg"
+  }
+]);
